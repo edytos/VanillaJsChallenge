f3b1c915a0dc12988de8724d4542c2e5de2fe65f
edytos <93355301+edytos@users.noreply.github.com>
2021-10-29 20:58:52 +0900

    Clock Modify

--- js/clock.js
+++ js/clock.js
@@ -1,3 +1,4 @@
+/*
 const clock = document.querySelector("h2#clock");
 
 function getClock() {
@@ -10,3 +11,47 @@ function getClock() {
 
 getClock();
 setInterval(getClock, 1000);
+*/
+
+const daysofweek = ["sun", "mon", "tus", "wed", "thu", "fri", "sat"];
+const month = [
+  "JAN",
+  "FEB",
+  "MAR",
+  "APR",
+  "MAY",
+  "JUN",
+  "JUL",
+  "AUG",
+  "SEP",
+  "OCT",
+  "NOV",
+  "DEC",
+];
+
+function clock() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  let day = h < 11 ? "AM" : "PM";
+  const daytoday = today.getDay();
+  const date = today.getDate();
+  const mon = today.getMonth();
+  const year = today.getFullYear();
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  document.getElementById("hours").innerHTML = h;
+  document.getElementById("min").innerHTML = m;
+  document.getElementById("sec").innerHTML = s;
+  document.getElementById("time").innerHTML = day;
+  document.getElementById("day").innerHTML = date;
+  document.getElementById("month").innerHTML = month[mon];
+  document.getElementById("year").innerHTML = year;
+  document.getElementById(daysofweek[daytoday]).style.color = "#fff";
+}
+
+const inter = setInterval(clock, 1000);
