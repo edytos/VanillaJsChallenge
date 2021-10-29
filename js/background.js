645ffd7804e47bce95743d2becda0ba3a742b134
edytos <93355301+edytos@users.noreply.github.com>
2021-10-29 16:44:28 +0900

    Change BG Images

--- js/background.js
+++ js/background.js
@@ -1,9 +1,5 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const images = ["0.png", "1.png", "2.png"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
-
-bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
+document.body.style.backgroundImage = `url("/img/${chosenImage}")`;
