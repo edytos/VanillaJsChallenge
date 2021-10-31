e97360025511c45dca155e4c6052a0234213964a
edytos <93355301+edytos@users.noreply.github.com>
2021-11-01 02:44:47 +0900

    small fix

--- js/greetings.js
+++ js/greetings.js
@@ -14,7 +14,7 @@ function onLoginSubmit(event) {
 }
 
 function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
+  greeting.innerText = `Have a nice day ${username} !`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
